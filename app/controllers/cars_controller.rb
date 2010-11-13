9602aebb7713174ff3e7ac6011599d90cb644b25
Owner <minikermit@ubuntu.(none)>
2010-11-13 23:22:04 +0100

    this is the second commit

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -22,7 +22,7 @@ class CarsController < ApplicationController
     end
 
     def sp
-    @cars = Car.find_by_sql('exec select_total 0')
+      @cars = Car.find_by_sql('exec select_total 0' )
 
     respond_to do |format|
       format.html # index.html.erb
