df980fd6b997dd6704ce5cd73b76fac45d54aa45
minikermit <skitter@genesis.com>
2011-01-29 23:49:36 +0100

    Move to rails 3

--- config/application.rb
+++ config/application.rb
@@ -24,11 +24,11 @@ module Sqlservapp
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Bern'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :en
 
     # JavaScript files you want as :defaults (application.js is always included).
     config.action_view.javascript_expansions[:defaults] = %w()
@@ -40,7 +40,8 @@ module Sqlservapp
     config.filter_parameters += [:password]
 
     config.generators do |g|
-	g.test_framework :rspec
+      # g.template_engine :haml
+	    g.test_framework :rspec, :fixture => false
     end
 
   end
